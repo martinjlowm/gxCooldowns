a76f223795dd02292f64a0de72c33b85df0cb906
Martin Jesper Low Madsen <martin_jlowm@hotmail.com>
2010-09-14 15:56:43 +0200

    Remove Divine Favor again.

--- cooldown.lua
+++ cooldown.lua
@@ -640,7 +640,6 @@ end
 local specialOccasions = {
 	[GetSpellInfo(14177)] = true,	-- Cold Blood
 	[GetSpellInfo(11129)] = true,	-- Combustion
-	[GetSpellInfo(20216)] = true,	-- Divine Favor
 	[GetSpellInfo(16166)] = true,	-- Elemental Mastery
 	[GetSpellInfo(14751)] = true,	-- Inner Focus
 	[GetSpellInfo(17116)] = true,	-- Nature's Swiftness
